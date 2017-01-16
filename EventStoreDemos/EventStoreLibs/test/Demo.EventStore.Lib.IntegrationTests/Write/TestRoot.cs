3b3774186476a2972e7f9a5b904994ad0a16d912
Kristofer Nissbrandt <kristofer.nissbrandt@squeed.com>
2017-01-16 17:26:53 +0100

    Fixed integration tests

--- EventStoreDemos/EventStoreLibs/test/Demo.EventStore.Lib.IntegrationTests/Write/TestRoot.cs
+++ EventStoreDemos/EventStoreLibs/test/Demo.EventStore.Lib.IntegrationTests/Write/TestRoot.cs
@@ -6,11 +6,17 @@
 
     public class TestRoot : IAggregateRoot
     {
-        public string Id { get; }
-        public int Version { get; }
+        public string Id { get; set; }
+        public int Version { get; set; }
         public Type AggregateRootType { get; }
 
         public void LoadFromHistoricalEvents(IEnumerable<IDomainEvent> domainEvents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IDomainEvent> UncommittedEvents { get; }
+        public void CommitEvents()
         {
             throw new NotImplementedException();
         }
